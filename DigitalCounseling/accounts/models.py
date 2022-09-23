2dab7bb0727e0ac5b7d4d08ffd6e293069742b03
Abigiya Fikremariam <abigiya.fikremariam@yahoo.com>
2022-09-24 00:30:25 +0300

    Start again with new views

--- DigitalCounseling/accounts/models.py
+++ DigitalCounseling/accounts/models.py
@@ -1,32 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    is_user = models.BooleanField(default=False)
-    is_therapist = models.BooleanField(default=False)
-
-
-class Question(models.Model):
-    question_text = models.TextField()
+# Question about hobbie when user get started
+class Hobbie_Question(AbstractUser):
+    hquestion_text = models.TextField()
 
     def __str__(self):
-        return self.question_text
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
-    answer_text = models.CharField(max_length=500)
-
-    def __str__(self):
-        return f'{self.question.question_text}-{self.answer_text}'
-
-
-class UserAnswer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    answer = models.ManyToManyField(Answer)
-
-    def __str__(self):
-        return f'{self.user.username}'   
-
+        return self.hquestion_text
 
 MARITAL_CHOICES = (
     (1, 'single'),
@@ -34,14 +14,8 @@ MARITAL_CHOICES = (
     (3, 'divorced')
 )
 
-
-EDUCATION_CHOICES = (
-    (1, 'BSC'),
-    (2, 'Masters'),
-    (3, 'Phd')
-)
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class UserProfile(AbstractUser):
+    user = models
     profile_picture = models.ImageField(upload_to='user_images', blank=True)
     first_name = models.CharField(max_length=200)
     middle_name = models.CharField(max_length=200)
@@ -55,18 +29,53 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-
-class TherapistProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='therapist_images')
-    first_name = models.CharField(max_length=200)
-    middle_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    education_level = models.CharField(max_length=10, choices=EDUCATION_CHOICES)
-    dob = models.DateField()
-    marital_status = models.CharField(max_length=20, choices=MARITAL_CHOICES)
-    emergency_contact = models.CharField(max_length=15)
-    pub_date = models.DateTimeField()
+# Question for an appointment
+class Appoint_Question(models.Model):
+    aquestion_text = models.TextField()
 
     def __str__(self):
-        return f'{self.user.username} profile'  
+        return self.aquestion_text
+
+class Hobbie_Answer(models.Model):
+    hquestion = models.ForeignKey(Hobbie_Question, related_name='hobbie', on_delete=models.CASCADE)
+    hanswer_text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.hquestion.hquestion_text}-{self.hanswer_text}'
+
+class Appoint_Answer(models.Model):
+    aquestion = models.ForeignKey(Appoint_Question, related_name='answer', on_delete=models.CASCADE)
+    aanswer_text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.aquestion.aquestion_text}-{self.aanswer_text}'
+
+
+class UserAnswer(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    answer = models.ManyToManyField(Appoint_Answer)
+
+    def __str__(self):
+        return f'{self.user.username}'   
+
+
+# EDUCATION_CHOICES = (
+#     (1, 'BSC'),
+#     (2, 'Masters'),
+#     (3, 'Phd')
+# )
+
+# class TherapistProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     profile_picture = models.ImageField(upload_to='therapist_images')
+#     first_name = models.CharField(max_length=200)
+#     middle_name = models.CharField(max_length=200)
+#     last_name = models.CharField(max_length=200)
+#     education_level = models.CharField(max_length=10, choices=EDUCATION_CHOICES)
+#     dob = models.DateField()
+#     marital_status = models.CharField(max_length=20, choices=MARITAL_CHOICES)
+#     emergency_contact = models.CharField(max_length=15)
+#     pub_date = models.DateTimeField()
+
+#     def __str__(self):
+#         return f'{self.user.username} profile'  
--- DigitalCounseling/accounts/views.py
+++ DigitalCounseling/accounts/views.py
@@ -1,16 +1,5 @@
-from django.shortcuts import render, redirect
-from .forms import UserSignUpForm
+from django.http import HttpResponse
 
-def index(response):
-    return HttpResponse("<h1> Welcome to Be Happy Here! </h1>")
-    
-def register(request):
-    form = UserSignUpForm()
-    if request.method == 'POST':
-        form = UserSignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-    context = {
-        'form':form
-    }
-    return render(request, 'accounts/register.html', context)
+
+def index(request):
+    return HttpResponse("Welcome to LSSYA! We are here for you!")
