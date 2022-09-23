62ba9225552a3b737a505b9d504506d48c3739cb
Abigiya Fikremariam <abigiya.fikremariam@yahoo.com>
2022-09-24 00:34:03 +0300

    re arrangement

--- DigitalCounseling/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-# from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- DigitalCounseling/accounts/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Welcome to LSSYA! We are here for you!")
